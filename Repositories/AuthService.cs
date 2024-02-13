5bdb660c51354e7393abeb73b38c37bf97b73e42
Your Name <you@example.com>
2024-02-13 13:46:14 -0600

    backend: finishing out signin method

--- Repositories/AuthService.cs
+++ Repositories/AuthService.cs
@@ -9,14 +9,19 @@ public class AuthService : IAuthService
 {
 
     private static ArkDbContext _context;
+    private static IConfiguration _config;
 
-    public AuthService(ArkDbContext context)
+    public AuthService(ArkDbContext context, IConfiguration config)
     {
+        _config = config;
         _context = context;
     }
 
     public User CreateUser(User user)
     {
+        var passwordHash = bcrypt.HashPassword(user.Password);
+        user.Password = passwordHash;
+
         _context.Add(user);
         _context.SaveChanges();
         return user;
@@ -24,6 +29,19 @@ public class AuthService : IAuthService
 
     public string SignIn(string userName, string password)
     {
-        throw new NotImplementedException();
+        var user = _context.Users.SingleOrDefault(x => x.UserName == userName);
+        var verified = false;
+
+        if(user != null)
+        {
+            verified = bcrypt.Verify(password, user.Password);
+        }
+
+        if(user == null || !verified)
+        {
+            return String.Empty;
+        }
+
+        return "";
     }
 }
